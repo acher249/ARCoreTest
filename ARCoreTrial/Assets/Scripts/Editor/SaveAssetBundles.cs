855d7df9a98c82e784b48d94ba493fde594deb0a
acher249 <32597382+acher249@users.noreply.github.com>
2018-08-08 11:29:26 -0400

    Added Project
    Added Project

---
+++ ARCoreTrial/Assets/Scripts/Editor/SaveAssetBundles.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        // Choose what platform to build the bundle out to
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ ARCoreTrial/Assets/Scripts/LoadObjectFromBundle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadObjectFromBundle : MonoBehaviour
+{
+    public string url;
+
+    public void DownloadModelOnClick()
+    {
+        StartCoroutine("DownloadObject");
+        Debug.Log("Clicked Button");
+
+    }
+
+ //   void Start ()
+	//{
+	//    StartCoroutine("DownloadObject");
+	//}
+
+    IEnumerator DownloadObject()
+    {
+
+        WWW www = new WWW(url);
+        yield return www;
+        AssetBundle assetBundle = www.assetBundle;
+        // I have a DemoScene Prefab inside my already created "room" AssetBundle
+        GameObject g = Instantiate(assetBundle.LoadAsset("DemoScene")) as GameObject;
+    }
+}
